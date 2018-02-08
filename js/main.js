d2cd98f7e0180ebee9f6ec4107de4ffffb523ecf
Leonardo Maldonado <leonardomso11@gmail.com>
2018-02-08 14:04:59 -0200

    Finishedgit status!

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 const todoList = document.querySelector('.topics .todo-list');
-
 const form = document.querySelector('.book');
 
-form.addEventListener('submit', (event) => {
+const taskList = document.querySelector('.tasks-list');
+const task = document.querySelector('.task');
+
+form.addEventListener('submit', addForm, false);
+task.addEventListener('submit', addTask, false);
+
+function addForm(event) {
     event.preventDefault();
 
     const value = form.querySelector('input[type="text"]').value;
@@ -10,5 +15,23 @@ form.addEventListener('submit', (event) => {
 
     li.textContent = value;
     todoList.appendChild(li);
+}
 
-});
+function addTask(event) {
+    event.preventDefault();
+
+    const value = task.querySelector('input[type="text"]').value;
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    const checkbox = document.createElement('input');
+
+    checkbox.type = 'checkbox';
+    checkbox.classList = 'regular-checkbox';
+
+    li.classList = 'item';
+    span.textContent = value;
+    li.appendChild(checkbox);
+    li.appendChild(span);
+
+    taskList.insertBefore(li, taskList.firstElementChild);
+}
